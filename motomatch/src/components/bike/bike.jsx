3fc83dbc2c67407ba9607fd4ca042964305565cd
kushal-source <kushalsha1208@gmail.com>
2025-04-11 10:09:59 +0530

    Second Commit Components

---
+++ motomatch/src/components/bike/bike.jsx
@@ -0,0 +1,14 @@
+import './bike.css'
+
+function Bike(){
+    return(<>
+        <section className="bike">
+            <div className="findBikeImg"></div>
+            <div className="searchBike">
+                <h1>Let's find your Thrill</h1>
+            </div>
+        </section>
+    </>)
+}
+
+export default Bike;
